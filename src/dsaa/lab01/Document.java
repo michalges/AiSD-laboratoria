3438f979af7b51ec6236c11085d92b3b9604e8de
michalges <105984108+michalges@users.noreply.github.com>
2025-03-06 08:49:10 +0100

    fixed loaddocument

--- src/dsaa/lab01/Document.java
+++ src/dsaa/lab01/Document.java
@@ -7,39 +7,54 @@ import java.util.Scanner;
 
 public class Document {
     public static void loadDocument(String name, Scanner scan) {
-        ArrayList<String> lines = new ArrayList<String>();
-        try {
-            File myObj = new File(name);
-            scan = new Scanner(myObj);
-            while (scan.hasNextLine()) {
-                String data = scan.nextLine();
-                lines.add(data);
+//        ArrayList<String> lines = new ArrayList<String>();
+//        try {
+//            File myObj = new File(name);
+//            scan = new Scanner(myObj);
+//            while (scan.hasNextLine()) {
+//                String data = scan.nextLine();
+//                lines.add(data);
+//            }
+//            scan.close();
+//        } catch (FileNotFoundException e) {
+//            System.out.println("An error occurred.");
+//            e.printStackTrace();
+//        }
+        ArrayList<String> words = new ArrayList<String>();
+        while (true) {
+            String line = scan.nextLine();
+            if (line.equals("eod")) {
+                break;
             }
-            scan.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            for (String word : line.split(" ")) {
+                words.add(word);
+            }
         }
 
-        for (String line : lines) {
-            String words[] = line.split(" ");
-            for (String word : words) {
-                if (correctLink(word)) {
-                    System.out.print(word.substring(5).toLowerCase());
-                }
+        for (String word : words) {
+            if (correctLink(word)) {
+                System.out.println(word.substring(5).toLowerCase());
             }
-            System.out.println();
         }
     }
 
     // accepted only small letters, capitalic letter, digits nad '_' (but not on the begin)
     public static boolean correctLink(String link) {
-        if (!link.startsWith("link=")) {
+        if (!link.toLowerCase().startsWith("link=")) {
             return false;
-        } else if (link.charAt(5) == '_') {
+        }
+        link = link.substring(5);
+        if (link.length() == 0) {
+            return false;
+        }
+        if (!Character.isLetter(link.charAt(0))) {
+            return false;
+        }
+        if (!link.matches("[A-Za-z0-9_]+")) {
             return false;
         }
         return true;
     }
+
 
 }
